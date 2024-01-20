6e10b17a828e3ade4edb631c9ab9c7bc4445b05e
Abdellahsyani <syaniabdos@gmail.com>
2024-01-20 10:33:17 +0100

    bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,11 +16,11 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
 				swap = array[j];
-				array[j] = array[j+1];
-				array[j+1] = swap;
+				array[j] = array[j + 1];
+				array[j + 1] = swap;
 			}
 		}
 		print_array(array, size);
