680f77b1a5c89882a2dca16b0384e9a7c5d9e458
Abdellahsyani <syaniabdos@gmail.com>
2024-01-22 15:06:17 +0100

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,7 +12,7 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, k;
 	int temp;
 
-	if (!array || !size)
+	if (array == NULL || size == 0)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
