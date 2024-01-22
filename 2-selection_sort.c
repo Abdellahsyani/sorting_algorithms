7ecdce36894bf6ed9515b8ddc3ada9d83dcf777c
Abdellahsyani <syaniabdos@gmail.com>
2024-01-22 15:02:42 +0100

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,22 +9,25 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j; 
-	int k, temp;
+	size_t i, j, k;
+	int temp;
+
 
 	for (i = 0; i < size - 1; i++)
 	{
-		k = i;
-		for (j = 1 + i; j < size; j++)
+		for (j = size - 1, k = i + 1; j > i; j--)
 		{
 			if (array[j] < array[k])
 			{
 				k = j;
 			}
 		}
-		temp = array[i];
-		array[i] = array[k];
-		array[k] = temp;
-		print_array(array, size);
+		if (array[i] > array[k])
+		{
+			temp = array[i];
+			array[i] = array[k];
+			array[k] = temp;
+			print_array(array, size);
+		}
 	}
 }
